cf7c8602301fba36aa070e131b2da427b2479c42
vagrant <seb.namek@gmail.com>
2017-10-18 21:23:53 +0000

    tweets can be added and loaded, height changes to 100% on load

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,22 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- $( document ).ready(function() {
+ $( document ).ready(() => {
+
+   $('main').css("height","100%")
 
    $('form').submit(function(ev){
      ev.preventDefault();
      var data = $(this).serialize();
      console.log(data);
-     debugger
      $.ajax({
        url: '/tweets',
        method: 'POST',
        data: data,
-       success: function () {
-         console.log(data);
+       success: function (data) {
+         loadTweets();
        }
      })
    })
+
+   function loadTweets() {
+     $.ajax({
+       url: '/tweets',
+       method: 'GET',
+       success: function (tweets) {
+         renderTweets(tweets);
+       }
+     })
+   };
+   loadTweets();
   });
 
 
@@ -30,10 +42,7 @@ function escape(str) {
 }
 
 function createTweetElement(tweet) {
-  var postDate =new Date(tweet.created_at);
-
   var safeHTML = `<p>${escape(tweet.content.text)}</p>`;
-
   var $tweet = `
       <article class="container displayed-tweet">
           <header class="container">
@@ -44,7 +53,7 @@ function createTweetElement(tweet) {
             <div class="tweet-body">
                 ${safeHTML}
             </div>
-          <footer>${postDate}
+          <footer>${parseHumanDate(tweet.created_at)} ago
             <i class="fa fa-flag" aria-hidden="true"></i>
             <i class="fa fa-retweet" aria-hidden="true"></i>
             <i class="fa fa-heart" aria-hidden="true"></i>
@@ -60,58 +69,34 @@ function renderTweets(tweet) {
   }
 };
 
-var tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "<script>alert('uh oh!');</script>"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }]
-renderTweets(tweetData);
-// $('.new-tweet').append($tweet);
+function parseHumanDate(timeCreated) {
+    var created = new Date(timeCreated);
+    var seconds = Math.floor((Date.now() - created) / 1000);
 
-// Days ago function below....
+    var interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+        return interval + ' years';
+    }
 
-//
-// Date.daysBetween = function( date1, date2 ) {
-//
-//   // Convert both dates to milliseconds
-//   var date2_ms = 1508347599300;
-//   var date1_ms = 1461116232227;
-//
-//   // Calculate the difference in milliseconds
-//   var difference_ms = date2_ms - date1_ms;
-//   //take out milliseconds
-//   difference_ms = difference_ms/1000;
-//   var seconds = Math.floor(difference_ms % 60);
-//   difference_ms = difference_ms/60;
-//   var minutes = Math.floor(difference_ms % 60);
-//   difference_ms = difference_ms/60;
-//   var hours = Math.floor(difference_ms % 24);
-//   var days = Math.floor(difference_ms/24);
-//
-//   return days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, and ' + seconds + ' seconds';
-// }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + ' months';
+    }
+
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + ' days';
+    }
+
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + ' hours';
+    }
+
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + ' minutes';
+    }
+
+    return Math.floor(seconds) + ' seconds';
+}
