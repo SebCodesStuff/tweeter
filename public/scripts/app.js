a62c1967dfb7c18ca1f1cd894c892a9f29e5ee72
vagrant <seb.namek@gmail.com>
2017-10-18 15:14:29 +0000

    About to start a major reordering of the new tweet section. It was al working from this point.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,42 @@ $( document ).ready(function() {
      $(this).find('i').toggle()
  });
 });
+
+function createTweetElement(tweet) {
+
+var $tweet = `<header class="container">
+          <img class="tweet-img" src=${tweetData.user.avatars.small} alt="User Avatar">
+          <h2>${tweetData.user.name}</h2>
+          <p class="right">${tweetData.user.handle}</p>
+        </header>
+          <article class="tweet-body">
+              ${tweetData.content.text}
+          </article>
+        <footer>${tweetData.created_at}
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+        </footer>
+      `
+$('.new-tweet').append($tweet);
+}
+
+var tweetData = {
+  "user": {
+            "name": "Newton",
+            "avatars":
+              {
+                "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+                "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+                "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+              },
+            "handle": "@SirIsaac"
+          },
+  "content": {
+                "text": "If I have seen further it is by standing on the shoulders of giants"
+              },
+              "created_at": 1461116232227
+            }
+var $tweet = createTweetElement(tweetData);
+console.log($tweet);
+$('.new-tweet').append($tweet);
