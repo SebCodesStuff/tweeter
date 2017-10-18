28f50a0b452bdd3a0fc82d8cb36e673bbd075090
vagrant <seb.namek@gmail.com>
2017-10-18 20:29:40 +0000

    Added an asynchronous post with Ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,15 +4,39 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// $( document ).ready(function() {
-//  $(".displayed-tweet").hover(function(){
-//      $(this).find('i').toggle()
-//  });
-// });
+ $( document ).ready(function() {
 
+   $('form').submit(function(ev){
+     ev.preventDefault();
+     var data = $(this).serialize();
+     $.ajax({
+       url: '/tweets',
+       method: 'POST',
+       data: data,
+       success: function () {
+         console.log(data);
+       }
+     })
+   }),
+
+   function loadTweets() {
+
+   }
+
+  });
+
+
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 function createTweetElement(tweet) {
-  var date =new Date(tweet.created_at);
+  var postDate =new Date(tweet.created_at);
+
+  var safeHTML = `<p>${escape(tweet.content.text)}</p>`;
+
   var $tweet = `
       <article class="container displayed-tweet">
           <header class="container">
@@ -21,9 +45,9 @@ function createTweetElement(tweet) {
             <p class="right">${tweet.user.handle}</p>
           </header>
             <div class="tweet-body">
-                ${tweet.content.text}
+                ${safeHTML}
             </div>
-          <footer>${date}
+          <footer>${postDate}
             <i class="fa fa-flag" aria-hidden="true"></i>
             <i class="fa fa-retweet" aria-hidden="true"></i>
             <i class="fa fa-heart" aria-hidden="true"></i>
@@ -39,35 +63,5 @@ function renderTweets(tweet) {
   }
 };
 
-var tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }]
 renderTweets(tweetData);
 // $('.new-tweet').append($tweet);
